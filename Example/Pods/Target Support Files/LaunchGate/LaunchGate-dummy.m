bdd4142cb5b30e889aaa279d7f6885915955304f
git <dtrenz@gmail.com>
2016-01-19 18:02:58 -0500

    Added empty CocoaPod project & scaffolding.

---
+++ Example/Pods/Target Support Files/LaunchGate/LaunchGate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LaunchGate : NSObject
+@end
+@implementation PodsDummy_LaunchGate
+@end
---
+++ Example/Pods/Target Support Files/LaunchGate/LaunchGate-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LaunchGateVersionNumber;
+FOUNDATION_EXPORT const unsigned char LaunchGateVersionString[];
+
