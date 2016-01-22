2bd4487d29bbdd7eae21f6f1468d35013d72100d
Dan Trenz <dtrenz@gmail.com>
2016-01-21 19:20:06 -0500

    Fixing more merge artifacts.
    oops :/

--- Example/Tests/LaunchGateRemoteFileManagerSpec.swift
+++ Example/Tests/LaunchGateRemoteFileManagerSpec.swift
@@ -1,79 +1,62 @@
 import Quick
 import Nimble
 
-<<<<<<< HEAD
 import Alamofire
 
 
-=======
->>>>>>> develop
 @testable import LaunchGate
 
 class LaunchGateRemoteFileManagerSpec: QuickSpec {
   override func spec() {
-<<<<<<< HEAD
-    
+
     let exampleURI = "https://www.launchgate.com/update.json"
-    
+
     describe("#init") {
-      
+
       it("the URI is stored in the remote file manager") {
         let remoteFileManager = LaunchGateRemoteFileManager(remoteFileURI: exampleURI)
-        
+
         expect(remoteFileManager.remoteFileURI.URLString) == exampleURI
       }
-      
+
     }
-    
+
     describe("#fetchRemoteFile") {
-      
+
       class MockLaunchGateRemoteFileManager : LaunchGateRemoteFileManager {
         var createRemoteFileRequestWasCalledWithRemoteFileURI = false
         var performRemoteFileRequestWasCalled = false
-        
+
         override func createRemoteFileRequest(uri: URLStringConvertible) -> Request? {
           if uri.URLString == "https://www.launchgate.com/update.json" {
             createRemoteFileRequestWasCalledWithRemoteFileURI = true
           }
-          
+
           return nil
         }
-        
+
         override func performRemoteFileRequest(request: Request, responseHandler: (response: AnyObject?) -> Void) {
           request.responseJSON { response in
             //
           }
         }
       }
-      
+
       let remoteFileManager = MockLaunchGateRemoteFileManager(remoteFileURI: exampleURI)
-      
+
       beforeEach {
         remoteFileManager.fetchRemoteFile { _ in }
       }
-      
+
       it("creates a request from the given URI") {
         expect(remoteFileManager.createRemoteFileRequestWasCalledWithRemoteFileURI) == true
       }
-      
+
       it("performs the remote file request") {
         expect(remoteFileManager.createRemoteFileRequestWasCalledWithRemoteFileURI) == true
       }
-      
+
     }
-    
-=======
-    describe("the update URI") {
-      
-      it("is configurable") {
-        let testURI = "https://www.launchgate.com/update.json"
-        
-        let remoteFileManager = LaunchGateRemoteFileManager(remoteFileURI: testURI)
-        
-        expect(remoteFileManager.remoteFileURI.URLString) == "https://www.launchgate.com/update.json"
-      }
-      
-    }
->>>>>>> develop
+
   }
 }
--- Example/Tests/LaunchGateSpec.swift
+++ Example/Tests/LaunchGateSpec.swift
@@ -3,57 +3,40 @@ import Nimble
 
 @testable import LaunchGate
 
-<<<<<<< HEAD
-
 class LaunchGateSpec: QuickSpec {
   override func spec() {
-    
+
     let exampleURI = "https://www.example.com/example.json"
-    
+
     describe("#init") {
       it("the URI is stored in the remote file manager") {
         let launchGate = LaunchGate(uri: exampleURI)
-        
+
         expect(launchGate.remoteFileManager.remoteFileURI.URLString) == exampleURI
       }
     }
-    
+
     describe("#performCheck") {
-      
+
       class MockLaunchGateRemoteFileManager : LaunchGateRemoteFileManager {
         var fetchRemoteFileWasCalled = false
-        
+
         override func fetchRemoteFile(callback: (AnyObject?) -> Void) {
           fetchRemoteFileWasCalled = true
         }
       }
-      
+
       it("calls LaunchGateRemoteFileManager#fetchRemoteFile") {
         let launchGate = LaunchGate(uri: exampleURI)
         let mockRemoteFileManager = MockLaunchGateRemoteFileManager(remoteFileURI: exampleURI)
         launchGate.remoteFileManager = mockRemoteFileManager
-        
+
         launchGate.performCheck()
-        
+
         expect(mockRemoteFileManager.remoteFileURI.URLString) == exampleURI
       }
-      
+
     }
-    
-=======
-class LaunchGateSpec: QuickSpec {
-  override func spec() {
-    describe("the update URI") {
-      
-      it("is configurable") {
-        let testURI = "https://www.launchgate.com/update.json"
-        
-        let launchGate = LaunchGate(uri: testURI)
-        
-        expect(launchGate.uri) == "https://www.launchgate.com/update.json"
-      }
-      
-    }
->>>>>>> develop
+
   }
 }
