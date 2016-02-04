c4a76f4c023f06b1626d6bdd46118d0c62ddaaf6
Dan Trenz <dtrenz@gmail.com>
2016-02-04 16:21:08 -0500

    Config JSON parsing completed.
    Includes:
     - Configuration model
     - Parser protocol
     - Default parser
     - Test coverage
     - Test config fixtures

---
+++ Pod/Classes/LaunchGateConfiguration.swift
@@ -0,0 +1,51 @@
+//
+//  LaunchGateConfiguration
+//  Pods
+//
+//  Created by Dan Trenz on 2/2/16.
+//
+//
+
+import Foundation
+
+
+public struct LaunchGateAlertConfiguration {
+  let message: String
+  let blocking: Bool
+
+  init(message: String, blocking: Bool) {
+    self.message = message
+    self.blocking = blocking
+  }
+}
+
+public struct LaunchGateUpdateConfiguration {
+  let version: String
+  let message: String
+
+  init(version: String, message: String) {
+    // maybe a string version type?
+    self.version = version
+    self.message = message
+  }
+}
+
+public struct LaunchGateConfiguration {
+
+  let alert: LaunchGateAlertConfiguration
+  let optionalUpdate: LaunchGateUpdateConfiguration
+  let requiredUpdate: LaunchGateUpdateConfiguration
+
+  public init(
+      alert: LaunchGateAlertConfiguration,
+      optionalUpdate: LaunchGateUpdateConfiguration,
+      requiredUpdate: LaunchGateUpdateConfiguration
+    ) {
+
+    self.alert = alert
+    self.optionalUpdate = optionalUpdate
+    self.requiredUpdate = requiredUpdate
+
+  }
+
+}
