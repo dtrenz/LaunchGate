6b051519dfae03f033d33ac9cb37fc31bf0d4872
Dan Trenz <dtrenz@gmail.com>
2016-02-04 16:29:07 -0500

    Config JSON parsing completed.
    - Configuration model
    - Parser protocol
    - Default parser
    - Test coverage
    - Test config fixtures

--- Pod/Classes/LaunchGate.swift
+++ Pod/Classes/LaunchGate.swift
@@ -9,18 +9,28 @@
 import Foundation
 
 
+internal typealias LaunchGateError = protocol<ErrorType, CustomStringConvertible>
+
 public class LaunchGate {
 
-  var remoteFileManager: LaunchGateRemoteFileManager
+  internal var remoteFileManager: LaunchGateRemoteFileManager
+  internal var parser: LaunchGateParser
 
   public init(uri: String) {
     remoteFileManager = LaunchGateRemoteFileManager(remoteFileURIString: uri)
+    parser = LaunchGateDefaultParser()
   }
 
   public func performCheck() {
-    remoteFileManager.fetchRemoteFile { (json) -> Void in
-      print("JSON: \(json)")
+    remoteFileManager.fetchRemoteFile { (jsonData) -> Void in
+      if let config = self.parser.parse(jsonData) {
+        print(config)
+      }
     }
+  }
+
+  public func setParser<T: LaunchGateParser>(parser: T) {
+    self.parser = parser
   }
 
 }
