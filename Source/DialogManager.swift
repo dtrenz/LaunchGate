ac896dda52fbbcd5fffe208f0098db624db96a0c
Nicolas <nicolas.charvoz@epitech.eu>
2018-09-04 09:35:03 +0200

    Delegate launchgate

--- Source/DialogManager.swift
+++ Source/DialogManager.swift
@@ -8,100 +8,111 @@
 
 import Foundation
 
+// Localization of strings
+
 protocol Dialogable {
-  var message: String { get }
+    var message: String { get }
 }
 
 class DialogManager {
 
-  typealias RememberableDialogSubject = Dialogable & Rememberable
+    typealias RememberableDialogSubject = Dialogable & Rememberable
 
-  enum DialogType {
-    case alert(blocking: Bool)
-    case optionalUpdate(updateURL: URL)
-    case requiredUpdate(updateURL: URL)
-  }
-
-  func displayAlertDialog(_ alertConfig: RememberableDialogSubject, blocking: Bool) {
-    let dialog = createAlertController(.alert(blocking: blocking), message: alertConfig.message)
-
-    displayAlertController(dialog) { () -> Void in
-      if !blocking {
-        Memory.remember(alertConfig)
-      }
+    enum DialogType {
+        case alert(blocking: Bool)
+        case optionalUpdate(updateURL: URL)
+        case requiredUpdate(updateURL: URL)
     }
-  }
 
-  func displayRequiredUpdateDialog(_ updateConfig: Dialogable, updateURL: URL) {
-    let dialog = createAlertController(.requiredUpdate(updateURL: updateURL), message: updateConfig.message)
+    var stringHandler: StringHandler?
 
-    displayAlertController(dialog, completion: nil)
-  }
-
-  func displayOptionalUpdateDialog(_ updateConfig: RememberableDialogSubject, updateURL: URL) {
-    let dialog = createAlertController(.optionalUpdate(updateURL: updateURL), message: updateConfig.message)
-
-    displayAlertController(dialog) { () -> Void in
-      Memory.remember(updateConfig)
+    init(withStringHandler stringHandler: StringHandler? = nil) {
+        self.stringHandler = stringHandler
     }
-  }
 
-  // MARK: Custom Alert Controllers
+    func displayAlertDialog(_ alertConfig: RememberableDialogSubject, blocking: Bool) {
+        let dialog = createAlertController(.alert(blocking: blocking),
+                                           message: stringHandler?.alertMessage ?? alertConfig.message)
 
-  func createAlertController(_ type: DialogType, message: String) -> UIAlertController {
-    let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        displayAlertController(dialog) { () -> Void in
+            if !blocking {
+                Memory.remember(alertConfig)
+            }
+        }
+    }
 
-    switch type {
-    case let .alert(blocking):
-        if !blocking {
+    func displayRequiredUpdateDialog(_ updateConfig: Dialogable, updateURL: URL) {
+        let dialog = createAlertController(.requiredUpdate(updateURL: updateURL),
+                                           message: stringHandler?.requiredUpdateMessage ?? updateConfig.message)
+
+        displayAlertController(dialog, completion: nil)
+    }
+
+    func displayOptionalUpdateDialog(_ updateConfig: RememberableDialogSubject, updateURL: URL) {
+        let dialog = createAlertController(.optionalUpdate(updateURL: updateURL),
+                                           message: stringHandler?.optionalUpdateMessage ?? updateConfig.message)
+
+        displayAlertController(dialog) { () -> Void in
+            Memory.remember(updateConfig)
+        }
+    }
+
+    // MARK: Custom Alert Controllers
+
+    func createAlertController(_ type: DialogType, message: String) -> UIAlertController {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+
+        switch type {
+        case let .alert(blocking):
+            if !blocking {
+                alertController.addAction(dismissActon())
+            }
+
+        case let .optionalUpdate(updateURL):
             alertController.addAction(dismissActon())
+            alertController.addAction(updateAction(updateURL))
+
+        case let .requiredUpdate(updateURL):
+            alertController.addAction(updateAction(updateURL))
         }
 
-    case let .optionalUpdate(updateURL):
-        alertController.addAction(dismissActon())
-        alertController.addAction(updateAction(updateURL))
-
-    case let .requiredUpdate(updateURL):
-        alertController.addAction(updateAction(updateURL))
+        return alertController
     }
 
-    return alertController
-  }
-
-  func displayAlertController(_ alert: UIAlertController, completion: (() -> Void)?) {
-    DispatchQueue.main.async { [] in
-      if let topViewController = self.topViewController() {
-        topViewController.present(alert, animated: true) {
-          if let completion = completion {
-            completion()
-          }
-        }
-      }
-    }
-  }
-
-  func topViewController() -> UIViewController? {
-    return UIApplication.shared.keyWindow?.rootViewController
-  }
-
-  // MARK: Custom Alert Actions
-
-  private func dismissActon() -> UIAlertAction {
-    return UIAlertAction(
-        title: NSLocalizedString("Dismiss", comment: "Button title for dismissing the update AlertView"),
-        style: .default) { _ in }
-  }
-
-  private func updateAction(_ updateURL: URL) -> UIAlertAction {
-    return UIAlertAction(
-        title: NSLocalizedString("Update", comment: "Button title for accepting the update AlertView"),
-        style: .default) { (_) -> Void in
-            if UIApplication.shared.canOpenURL(updateURL) {
-                DispatchQueue.main.async { [] in
-                    UIApplication.shared.openURL(updateURL)
+    func displayAlertController(_ alert: UIAlertController, completion: (() -> Void)?) {
+        DispatchQueue.main.async { [] in
+            if let topViewController = self.topViewController() {
+                topViewController.present(alert, animated: true) {
+                    if let completion = completion {
+                        completion()
+                    }
                 }
             }
         }
-  }
+    }
+
+    func topViewController() -> UIViewController? {
+        return UIApplication.shared.keyWindow?.rootViewController
+    }
+
+    // MARK: Custom Alert Actions
+
+    private func dismissActon() -> UIAlertAction {
+        return UIAlertAction(
+            title: NSLocalizedString(stringHandler?.dismissTitle ?? "Dismiss", comment: "Button title for dismissing the update AlertView"),
+            style: .default) { _ in }
+    }
+
+    private func updateAction(_ updateURL: URL) -> UIAlertAction {
+        return UIAlertAction(
+            title: NSLocalizedString(stringHandler?.downloadTitle ?? "Update", comment: "Button title for accepting the update AlertView"),
+            style: .default) { (_) -> Void in
+                if UIApplication.shared.canOpenURL(updateURL) {
+                    DispatchQueue.main.async { [] in
+                        UIApplication.shared.openURL(updateURL)
+                    }
+                }
+        }
+    }
 
 }
--- Source/LaunchGate.swift
+++ Source/LaunchGate.swift
@@ -8,127 +8,140 @@
 
 import Foundation
 
+/// Protocol for LaunchGate
+public protocol LaunchGateDelegate: class {
+    func noUpdateHandler()
+}
+
 /// Custom internal error type
 typealias LaunchGateError = Error & CustomStringConvertible
 
 public class LaunchGate {
 
-  /// Parser to use when parsing the configuration file
-  public var parser: LaunchGateParser!
+    /// Parser to use when parsing the configuration file
+    public var parser: LaunchGateParser!
 
-  /// URI for the configuration file
-  var configurationFileURL: URL!
+    /// URI for the configuration file
+    var configurationFileURL: URL!
 
-  /// App Store URI ("itms-apps://itunes.apple.com/...") for the current app
-  var updateURL: URL!
+    /// App Store URI ("itms-apps://itunes.apple.com/...") for the current app
+    var updateURL: URL!
 
-  /// Manager object for the various alert dialogs
-  var dialogManager: DialogManager!
+    /// Manager object for the various alert dialogs
+    var dialogManager: DialogManager!
 
-  // MARK: - Public API
+    // MARK: - Public API
 
-  /**
-  Failable initializer. If either the `configURI` or `appStoreURI` are unable to be
-  converted into an `URL` (i.e. containing illegal URL characters) this initializer
-  will return `nil`.
+    weak var delegate: LaunchGateDelegate?
 
-  - Parameters:
-    - configURI: URI for the configuration file
-    - appStoreURI: App Store URI ("itms-apps://itunes.apple.com/...") for the current app
+    /**
+     Failable initializer. If either the `configURI` or `appStoreURI` are unable to be
+     converted into an `URL` (i.e. containing illegal URL characters) this initializer
+     will return `nil`.
 
-  - Returns: A `LaunchGate` instance or `nil`
-  */
-  public init?(configURI: String, appStoreURI: String) {
-    guard let configURL = URL(string: configURI) else { return nil }
-    guard let appStoreURL = URL(string: appStoreURI) else { return nil }
+     - Parameters:
+     - configURI: URI for the configuration file
+     - appStoreURI: App Store URI ("itms-apps://itunes.apple.com/...") for the current app
 
-    configurationFileURL = configURL
-    updateURL = appStoreURL
-    parser = DefaultParser()
-    dialogManager = DialogManager()
-  }
+     - Returns: A `LaunchGate` instance or `nil`
+     */
+    public init?(configURI: String, appStoreURI: String, stringHandler: StringHandler? = nil) {
+        guard let configURL = URL(string: configURI) else { return nil }
+        guard let appStoreURL = URL(string: appStoreURI) else { return nil }
 
-  /// Check the configuration file and perform any appropriate action.
-  public func check() {
-    performCheck(RemoteFileManager(remoteFileURL: (configurationFileURL as URL)))
-  }
-
-  // MARK: - Internal API
-
-  /**
-  Check the configuration file and perform any appropriate action, using
-  the provided `RemoteFileManager`.
-
-  - Parameter remoteFileManager: The `RemoteFileManager` to use to fetch the configuration file.
-  */
-  func performCheck(_ remoteFileManager: RemoteFileManager) {
-    remoteFileManager.fetchRemoteFile { (jsonData) -> Void in
-      if let config = self.parser.parse(jsonData) {
-        self.displayDialogIfNecessary(config, dialogManager: self.dialogManager)
-      }
+        configurationFileURL = configURL
+        updateURL = appStoreURL
+        parser = DefaultParser()
+        dialogManager = DialogManager(withStringHandler: stringHandler)
     }
-  }
 
-  /**
-   Determine which dialog, if any, to display based on the parsed configuration.
+    public func setDelegate(_ delegate: LaunchGateDelegate?) {
+        self.delegate = delegate
+    }
 
-   - Parameters:
+    /// Check the configuration file and perform any appropriate action.
+    public func check() {
+        performCheck(RemoteFileManager(remoteFileURL: (configurationFileURL as URL)))
+    }
+
+    // MARK: - Internal API
+
+    /**
+     Check the configuration file and perform any appropriate action, using
+     the provided `RemoteFileManager`.
+
+     - Parameter remoteFileManager: The `RemoteFileManager` to use to fetch the configuration file.
+     */
+    func performCheck(_ remoteFileManager: RemoteFileManager) {
+        remoteFileManager.fetchRemoteFile { (jsonData) -> Void in
+            if let config = self.parser.parse(jsonData) {
+                self.displayDialogIfNecessary(config, dialogManager: self.dialogManager)
+            }
+        }
+    }
+
+    /**
+     Determine which dialog, if any, to display based on the parsed configuration.
+
+     - Parameters:
      - config:        Configuration parsed from remote configuration file.
      - dialogManager: Manager object for the various alert dialogs
-   */
-  func displayDialogIfNecessary(_ config: LaunchGateConfiguration, dialogManager: DialogManager) {
-    if let reqUpdate = config.requiredUpdate, let appVersion = currentAppVersion() {
-      if shouldShowRequiredUpdateDialog(reqUpdate, appVersion: appVersion) {
-        dialogManager.displayRequiredUpdateDialog(reqUpdate, updateURL: updateURL)
-      }
-    } else if let optUpdate = config.optionalUpdate, let appVersion = currentAppVersion() {
-      if shouldShowOptionalUpdateDialog(optUpdate, appVersion: appVersion) {
-        dialogManager.displayOptionalUpdateDialog(optUpdate, updateURL: updateURL)
-      }
-    } else if let alert = config.alert {
-      if shouldShowAlertDialog(alert) {
-        dialogManager.displayAlertDialog(alert, blocking: alert.blocking)
-      }
+     */
+    func displayDialogIfNecessary(_ config: LaunchGateConfiguration, dialogManager: DialogManager) {
+        if let reqUpdate = config.requiredUpdate, let appVersion = currentAppVersion() {
+            if shouldShowRequiredUpdateDialog(reqUpdate, appVersion: appVersion) {
+                dialogManager.displayRequiredUpdateDialog(reqUpdate, updateURL: updateURL)
+            }
+        } else if let optUpdate = config.optionalUpdate, let appVersion = currentAppVersion() {
+            if shouldShowOptionalUpdateDialog(optUpdate, appVersion: appVersion) {
+                dialogManager.displayOptionalUpdateDialog(optUpdate, updateURL: updateURL)
+            }
+        } else if let alert = config.alert {
+            if shouldShowAlertDialog(alert) {
+                dialogManager.displayAlertDialog(alert, blocking: alert.blocking)
+            }
+        } else {
+            delegate?.noUpdateHandler()
+        }
     }
-  }
 
-  /**
-   Determine if an alert dialog should be displayed, based on the configuration.
+    /**
+     Determine if an alert dialog should be displayed, based on the configuration.
 
-   - Parameter alertConfig: An `AlertConfiguration`, parsed from the configuration file.
+     - Parameter alertConfig: An `AlertConfiguration`, parsed from the configuration file.
 
-   - Returns: `true`, if an alert dialog should be displayed; `false`, if not.
-   */
-  func shouldShowAlertDialog(_ alertConfig: AlertConfiguration) -> Bool {
-    return alertConfig.blocking || alertConfig.isNotRemembered()
-  }
+     - Returns: `true`, if an alert dialog should be displayed; `false`, if not.
+     */
+    func shouldShowAlertDialog(_ alertConfig: AlertConfiguration) -> Bool {
+        return alertConfig.blocking || alertConfig.isNotRemembered()
+    }
 
-  /**
-   Determine if an optional update dialog should be displayed, based on the configuration.
+    /**
+     Determine if an optional update dialog should be displayed, based on the configuration.
 
-   - Parameter updateConfig: An `UpdateConfiguration`, parsed from the configuration file.
+     - Parameter updateConfig: An `UpdateConfiguration`, parsed from the configuration file.
 
-   - Returns: `true`, if an optional update should be displayed; `false`, if not.
-   */
-  func shouldShowOptionalUpdateDialog(_ updateConfig: UpdateConfiguration, appVersion: String) -> Bool {
-    guard updateConfig.isNotRemembered() else { return false }
+     - Returns: `true`, if an optional update should be displayed; `false`, if not.
+     */
+    func shouldShowOptionalUpdateDialog(_ updateConfig: UpdateConfiguration, appVersion: String) -> Bool {
+        guard updateConfig.isNotRemembered() else { return false }
 
-    return appVersion < updateConfig.version
-  }
+        return appVersion < updateConfig.version
+    }
 
-  /**
-   Determine if a required update dialog should be displayed, based on the configuration.
+    /**
+     Determine if a required update dialog should be displayed, based on the configuration.
 
-   - Parameter updateConfig: An `UpdateConfiguration`, parsed from the configuration file.
+     - Parameter updateConfig: An `UpdateConfiguration`, parsed from the configuration file.
 
-   - Returns: `true`, if a required update dialog should be displayed; `false`, if not.
-   */
-  func shouldShowRequiredUpdateDialog(_ updateConfig: UpdateConfiguration, appVersion: String) -> Bool {
-    return appVersion < updateConfig.version
-  }
+     - Returns: `true`, if a required update dialog should be displayed; `false`, if not.
+     */
+    func shouldShowRequiredUpdateDialog(_ updateConfig: UpdateConfiguration, appVersion: String) -> Bool {
+        return appVersion < updateConfig.version
+    }
 
-  func currentAppVersion() -> String? {
-    return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-  }
+    func currentAppVersion() -> String? {
+        return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    }
 
 }
