e7515428fd6da66ae23ab9fdc594558860f4a2f5
Britney Smith <brit.smith7@gmail.com>
2018-11-15 17:16:30 -0500

    WIP - at this point, data is parsed, still with JSONSerialization

--- Source/DefaultParser.swift
+++ Source/DefaultParser.swift
@@ -50,27 +50,21 @@ class DefaultParser: LaunchGateParser {
       var alert: AlertConfiguration?
       var optionalUpdate: UpdateConfiguration?
       var requiredUpdate: UpdateConfiguration?
-
-        guard let alertJSON = config["alert"] as? Data else {
-            print("No alert")
-            return nil }
+        
+      if let alertJSON = config["alert"] as? JSON {
         alert = try DefaultParser.parseAlert(alertJSON)
         print("Alert: \(alert!)")
-      
+      }
 
-      guard let optionalUpdateJSON = config["optionalUpdate"] as? Data else {
-        print("No optional update")
-        return nil }
+      if let optionalUpdateJSON = config["optionalUpdate"] as? JSON {
         optionalUpdate = try DefaultParser.parseOptionalUpdate(optionalUpdateJSON)
         print("Optional Update: \(optionalUpdate!)")
-      
+      }
 
-      guard let requiredUpdateJSON = config["requiredUpdate"] as? Data else {
-        print("No required update")
-        return nil }
+      if let requiredUpdateJSON = config["requiredUpdate"] as? JSON {
         requiredUpdate = try DefaultParser.parseRequiredUpdate(requiredUpdateJSON)
         print("Required Update: \(requiredUpdate!)")
-      
+      }
 
       return LaunchGateConfiguration(alert: alert, optionalUpdate: optionalUpdate, requiredUpdate: requiredUpdate)
     } catch let error as DefaultParser.Error {
@@ -86,56 +80,56 @@ class DefaultParser: LaunchGateParser {
     return nil
   }
 //   --- Experiments ---
-    private static func parseAlert(_ jsonData: Data) -> AlertConfiguration? {
-        do {
-            let decoder = JSONDecoder()
-            let result = try decoder.decode(AlertConfiguration.self, from: jsonData)
-            return result
-        } catch {
-            print(error)
-            return nil
-        }
-    }
-    private static func parseOptionalUpdate(_ jsonData: Data) -> UpdateConfiguration? {
-        do {
-            let decoder = JSONDecoder()
-            let result = try decoder.decode(UpdateConfiguration.self, from: jsonData)
-            return result
-        } catch {
-            print(error)
-            return nil
-        }
-    }
-    private static func parseRequiredUpdate(_ jsonData: Data) -> UpdateConfiguration? {
-        do {
-            let decoder = JSONDecoder()
-            let result = try decoder.decode(UpdateConfiguration.self, from: jsonData)
-            return result
-        } catch {
-            print(error)
-            return nil
-        }
-    }
+//    private static func parseAlert(_ jsonData: Data) -> AlertConfiguration? {
+//        do {
+//            let decoder = JSONDecoder()
+//            let result = try decoder.decode(AlertConfiguration.self, from: jsonData)
+//            return result
+//        } catch {
+//            print(error)
+//            return nil
+//        }
+//    }
+//    private static func parseOptionalUpdate(_ jsonData: Data) -> UpdateConfiguration? {
+//        do {
+//            let decoder = JSONDecoder()
+//            let result = try decoder.decode(UpdateConfiguration.self, from: jsonData)
+//            return result
+//        } catch {
+//            print(error)
+//            return nil
+//        }
+//    }
+//    private static func parseRequiredUpdate(_ jsonData: Data) -> UpdateConfiguration? {
+//        do {
+//            let decoder = JSONDecoder()
+//            let result = try decoder.decode(UpdateConfiguration.self, from: jsonData)
+//            return result
+//        } catch {
+//            print(error)
+//            return nil
+//        }
+//    }
 //  --- End of Experiments ---
-//  private static func parseAlert(_ json: JSON) throws -> AlertConfiguration? {
-//    guard let message = json["message"] as? String else { throw Error.unableToParseAlert }
-//    guard let blocking = json["blocking"] as? Bool else { throw Error.unableToParseAlert }
-//
-//    return AlertConfiguration(message: message, blocking: blocking)
-//  }
-//
-//  private static func parseOptionalUpdate(_ json: JSON) throws -> UpdateConfiguration? {
-//    guard let version = json["optionalVersion"] as? String else { throw Error.unableToParseOptionalUpdate }
-//    guard let message = json["message"] as? String else { throw Error.unableToParseOptionalUpdate }
-//
-//    return UpdateConfiguration(version: version, message: message)
-//  }
-//
-//  private static func parseRequiredUpdate(_ json: JSON) throws -> UpdateConfiguration? {
-//    guard let version = json["minimumVersion"] as? String else { throw Error.unableToParseRequiredUpdate }
-//    guard let message = json["message"] as? String else { throw Error.unableToParseRequiredUpdate }
-//
-//    return UpdateConfiguration(version: version, message: message)
-//  }
+  private static func parseAlert(_ json: JSON) throws -> AlertConfiguration? {
+    guard let message = json["message"] as? String else { throw Error.unableToParseAlert }
+    guard let blocking = json["blocking"] as? Bool else { throw Error.unableToParseAlert }
+
+    return AlertConfiguration(message: message, blocking: blocking)
+  }
+
+  private static func parseOptionalUpdate(_ json: JSON) throws -> UpdateConfiguration? {
+    guard let version = json["optionalVersion"] as? String else { throw Error.unableToParseOptionalUpdate }
+    guard let message = json["message"] as? String else { throw Error.unableToParseOptionalUpdate }
+
+    return UpdateConfiguration(version: version, message: message)
+  }
+
+  private static func parseRequiredUpdate(_ json: JSON) throws -> UpdateConfiguration? {
+    guard let version = json["minimumVersion"] as? String else { throw Error.unableToParseRequiredUpdate }
+    guard let message = json["message"] as? String else { throw Error.unableToParseRequiredUpdate }
+
+    return UpdateConfiguration(version: version, message: message)
+  }
 
 }
